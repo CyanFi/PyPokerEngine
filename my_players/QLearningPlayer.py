9f61e6a72e9cdbc4229683b8eca01e2a7e90845d
Cyan-Fi <qingyuge006@gmail.com>
2020-09-18 20:17:02 +0800

    rl player set up

---
+++ my_players/QLearningPlayer.py
@@ -0,0 +1,113 @@
+# GOOD GOOD STUDY, DAY DAY UP!
+#    @Time:    9/18/2020 5:03 PM
+#    @Author:  Qingy
+#    @Email:   qingyuge006@gmail.com
+#    @File:    QLearningPlayer.py
+#    @Project: pypokerengine
+from pypokerengine.players import BasePokerPlayer
+import random as rand
+import numpy as np
+from pypokerengine.utils.card_utils import gen_cards, estimate_hole_card_win_rate
+
+
+class QLearningPlayer(BasePokerPlayer):
+
+    def __init__(self, path):
+        '''
+        Q: hand_strength, bid_blind_pos, action
+        '''
+        self.fold_ratio = self.call_ratio = raise_ratio = 1.0 / 3
+        self.nb_player = None
+        self.Q = np.zeros(11 * 2 * 4).reshape(11, 2, 4)
+        # load modules:
+        try:
+            self.Q = np.load(path)
+        except:
+            pass
+        # hyper-parameter for q learning
+        self.epislon = 0.1
+        self.gamma = 0.9
+        self.learning_rate = 0.05
+        self.num_simulation = 1000
+        self.hand_strength = 0
+        self.hole_card = None
+        self.model_path = path
+        self.history = []
+
+    def set_action_ratio(self, fold_ratio, call_ratio, raise_ratio):
+        ratio = [fold_ratio, call_ratio, raise_ratio]
+        scaled_ratio = [1.0 * num / sum(ratio) for num in ratio]
+        self.fold_ratio, self.call_ratio, self.raise_ratio = scaled_ratio
+
+    @staticmethod
+    def action_to_int(action):
+        if action == 'fold':
+            return 0
+        if action == 'check':
+            return 1
+        if action == 'call':
+            return 2
+        if action == 'raise':
+            return 3
+
+    def declare_action(self, valid_actions, hole_card, round_state):
+        self.hand_strength = estimate_hole_card_win_rate(nb_simulation=self.num_simulation,
+                                                         nb_player=self.nb_player,
+                                                         hole_card=gen_cards(hole_card),
+                                                         community_card=gen_cards(round_state['community_card']))
+        state = int(self.hand_strength * 10), round_state['big_blind_pos']
+        max_a = 0
+        max_q = -100000
+        for a in valid_actions:
+            tmp_a = self.action_to_int(a['action'])
+            i=state+(tmp_a,)
+            if self.Q[i] > max_q:
+                max_a = a
+                max_q = self.Q[i]
+        choice = max_a
+        action = choice["action"]
+        amount = choice["amount"]
+        if action == "raise":
+            # To simpilify the problem, raise only at minimum
+            amount = amount["min"]
+        # record the action
+        self.history.append(state+(self.action_to_int(action),))
+        return action, amount
+
+    def __choice_action(self, valid_actions):
+        r = rand.random()
+        if r <= self.fold_ratio:
+            return valid_actions[0]
+        elif r <= self.call_ratio:
+            return valid_actions[1]
+        else:
+            return valid_actions[2]
+
+    def receive_game_start_message(self, game_info):
+        self.nb_player = game_info['player_num']
+
+    def receive_round_start_message(self, round_count, hole_card, seats):
+        pass
+
+    def receive_street_start_message(self, street, round_state):
+        pass
+
+    def receive_game_update_message(self, new_action, round_state):
+        pass
+
+    def receive_round_result_message(self, winners, hand_info, round_state):
+        if self.history:
+            self.gamma=1.0/round_state['round_count']
+            # player has declared some action before
+            # define the reward
+            if winners[0]['uuid']==self.uuid:
+                reward = winners[0]['stack'] - 100
+            else:
+                reward = 100- winners[0]['stack']
+            # reward all history actions
+
+            for h in self.history:
+                learning_target = reward + self.gamma * self.Q[h]
+                self.Q[h] = self.Q[h] + self.learning_rate * learning_target
+            # save the model
+            np.save(self.model_path, self.Q)
