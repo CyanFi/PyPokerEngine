02627e99ce4e605d65c6d6d497a99c1df060fb1c
Cyan Fisher <fisher@Cyans-MacBook-Pro.local>
2020-11-06 17:44:22 +0800

    [a2c] first commit

--- train.py
+++ train.py
@@ -2,16 +2,15 @@
 #    @Time:    9/18/2020 4:52 PM
 #    @Author:  Qingy
 #    @Email:   qingyuge006@gmail.com
-#    @File:    train.py.py
+#    @File:    train.py
 #    @Project: pypokerengine
 from pypokerengine.api.game import setup_config, start_poker
 from my_players.RandomPlayer import RandomPlayer
 from my_players.QLearningPlayer import QLearningPlayer
 from my_players.HumanPlayer import ConsolePlayer
-from my_players.HonestPlayer import HonestPlayer
-from my_players.AllCallPlayer import AllCallPlayer
 from my_players.DQNPlayer import DQNPlayer
 from my_players.cardplayer import cardplayer
+from my_players.A2CPlayer import A2CPlayer
 from scipy.stats import t
 import math
 
@@ -20,8 +19,8 @@ win = 0
 sample_mean = 0
 SXX = 0
 sample_std = 0
-path0 = 'model/DQN_card_1.npy'
-path1 = 'model/DQN4_optim_1.dump'
+path0 = 'model/a2c.dump'
+path1 = 'model/a2c_optim.dump'
 count = 0
 log_interval = 10
 log = []
@@ -29,12 +28,13 @@ confidence_level = 0.95
 memory = None
 config = setup_config(max_round=100, initial_stack=1500, small_blind_amount=5)
 config.register_player(name="p1", algorithm=cardplayer())
-config.register_player(name="p2", algorithm=DQNPlayer(model_path=path0,optimizer_path=path1,training=True))
-config.players_info[1]['algorithm'].oponent = config.players_info[0]['algorithm']
+config.register_player(name="p2", algorithm=A2CPlayer(model_path=path0,optimizer_path=path1,training=True))
+# config.players_info[1]['algorithm'].oponent = config.players_info[0]['algorithm']
 for i in range(0, num_episode):
     count = count + 1
     game_result = start_poker(config, verbose=0)
     win = (game_result['players'][1]['stack'] - game_result['players'][0]['stack']) / 2 / 10
+    # calculate bb/100g
     last_mean = sample_mean
     sample_mean = sample_mean + (win - sample_mean) / count
     SXX = SXX + (win - sample_mean) * (win - last_mean)
@@ -44,7 +44,7 @@ for i in range(0, num_episode):
     log.append((count,sample_mean))
     #log_loss.append(config.players_info[1]['algorithm'].loss)
     if count % log_interval == 0:
-        print(count, ' episode, 百手盈利', sample_mean, u"\u00B1", (interval[1] - interval[0]) / 2,"loss: ",config.players_info[1]['algorithm'].loss)
+        print(count, ' episode, 百手盈利', sample_mean, u"\u00B1", (interval[1] - interval[0]) / 2)
 log_loss = config.players_info[1]['algorithm'].loss
 # plot
 import matplotlib.pyplot as plt
